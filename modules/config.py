c27b611a08e9206cc54d093e51384359a574316b
donmunna435-dev <donmunna435@gmail.com>
2026-01-08 10:40:51 +0530

    Add files via upload

---
+++ modules/config.py
@@ -0,0 +1,6 @@
+# config.py
+BOT_NAME = "ROWDY"
+BOT_TOKEN = "8266282399:AAHQapxFlhTrl0nd_zr5CfnOLCnqxNaBlYs"
+API_ID = "24754824"
+API_HASH = "e24a9c7a6aa24e1c56fa349e104ec20e"
+MONGO_URI = "mongodb+srv://rowdybro013:DmnZ3s14aRbfA8tb@cluster01.ramjm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster01"
